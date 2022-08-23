0873d2b74ad8f6d282503111d42785211bfd14db
liding <539561425@qq.com>
2022-08-24 00:19:13 +0800

    sort.Slice(intSlice, func(i, j int)bool{})

---
+++ awesomeProject/order.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strconv"
+	// "strings"
+)
+
+func main() {
+	intSlice := []int{8, 90, 3, 303, 56, 74}
+	sort.Slice(intSlice, func(i, j int) bool {
+		a := strconv.Itoa(intSlice[i])
+		b := strconv.Itoa(intSlice[j])
+		return a+b > b+a
+	})
+	fmt.Println(intSlice)
+	// res := strings.Join(intSlice, )
+	// fmt.Println(res)
+	str := ""
+	for i := 0; i < len(intSlice); i++ {
+		str += strconv.Itoa(intSlice[i])
+	}
+	fmt.Println(str)
+}
