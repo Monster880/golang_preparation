013418bf104a5302fbd29dc07958138498d955f7
liding <539561425@qq.com>
2022-09-03 23:43:09 +0800

    fix

--- infixtoprefix.go
+++ infixtoprefix.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
+	fmt.Println("(3+4)*5-6")
 	fmt.Println(in2pre([]rune("(3+4)*5-6")))
 }
 
@@ -24,17 +25,16 @@ func in2pre(s []rune) string {
 	num := make([]rune, 0)
 	operator := make([]rune, 0)
 	for i := len(s) - 1; i >= 0; i-- {
-		fmt.Println(string(s[i]))
 		if s[i] >= '0' && s[i] <= '9' {
 			num = append(num, s[i])
 		} else if len(operator) == 0 || priority(string(s[i])) >= priority(string(operator[len(operator)-1])) {
 			operator = append(operator, s[i])
 		} else {
-			for len(operator) > 0 && operator[len(operator)-1] != ')' {
+			for len(operator) > 0 && string(operator[len(operator)-1]) != ")" {
 				num = append(num, operator[len(operator)-1])
 				operator = operator[:len(operator)-1]
 			}
-			if s[i] == '(' {
+			if string(s[i]) == "(" {
 				operator = operator[:len(operator)-1]
 			} else {
 				operator = append(operator, s[i])
